d22a86d86fd6c9660a218ef8175e6ece784cd3db
CompSciCardwell <bcardwell1@student.gsu.edu>
2023-04-21 11:25:06 -0400

    Initial Commit

---
+++ src/main/java/com/cardwell/MagMutualTest/MagMutualTestApplication.java
@@ -0,0 +1,13 @@
+package com.cardwell.MagMutualTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagMutualTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagMutualTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardwell/MagMutualTest/MagMutualTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardwell.MagMutualTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagMutualTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
